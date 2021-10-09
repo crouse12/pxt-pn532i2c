29f0aef5af8109363a9956055210f8744d6eaab7
Chris Torrence <chris_torrence@comcast.net>
2021-10-08 19:41:41 -0600

    add GetUUID block

--- rfid.ts
+++ rfid.ts
@@ -72,60 +72,13 @@ const enum LcdPosition1602 {
 namespace makerbit {
 
   /**
-   * Displays a text on a LCD1602 in the given position range.
-   * The text will be cropped if it is longer than the provided length.
-   * If there is space left, it will be filled with pad characters.
-   * @param text the text to show, eg: "MakerBit"
-   * @param startPosition the start position on the LCD, [1 - 32]
-   * @param length the maximum space used on the LCD, eg: 16
-   * @param option configures padding and alignment, eg: TextOption.Left
-   */
-  //% subcategory="LCD"
-  //% blockId="makerbit_lcd_show_string_on_1602"
-  //% block="LCD1602 show %text | at position %startPosition=makerbit_lcd_position_1602 with length %length || and %option"
-  //% text.shadowOptions.toString=true
-  //% length.min=1 length.max=32 length.fieldOptions.precision=1
-  //% expandableArgumentMode="toggle"
-  //% inlineInputMode="inline"
-  //% weight=90
-  export function showStringOnLcd1602(
-    text: string,
-    startPosition: number,
-    length: number,
-    option?: TextOption
-  ): void {
-    updateCharacterBuffer(
-      text,
-      startPosition - 1,
-      length,
-      16,
-      2,
-      toAlignment(option),
-      toPad(option)
-    );
-  }
-
-  /**
    * Clears the LCD1602 completely.
    */
-  //% subcategory="LCD"
-  //% blockId="makerbit_lcd_clear_1602" block="LCD1602 clear display"
+  //% subcategory="RFID"
+  //% blockId="makerbit_rfid_get_uuid" block="RFID UUID"
   //% weight=89
-  export function clearLcd1602(): void {
-    showStringOnLcd1602("", 1, 32);
+  export function rfidGetUUID(): void {
+//    showStringOnLcd1602("", 1, 32);
   }
 
-  /**
-   * Turns a LCD position into a number.
-   * @param pos the LCD position, eg: LcdPosition1602.Pos1
-   */
-  //% subcategory="LCD"
-  //% blockId=makerbit_lcd_position_1602
-  //% block="%pos"
-  //% pos.fieldEditor="gridpicker"
-  //% pos.fieldOptions.columns=16
-  //% blockHidden=true
-  export function position1602(pos: LcdPosition1602): number {
-    return pos;
-  }
 }
