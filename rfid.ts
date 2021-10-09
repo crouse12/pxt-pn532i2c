42f2d1ca5578907e9ce2cf1f4d5b660571f5d430
Chris Torrence <chris_torrence@comcast.net>
2021-10-09 14:58:22 -0600

    add index param

--- rfid.ts
+++ rfid.ts
@@ -6,15 +6,15 @@
 namespace makerbit {
 
   /**
-   * Get the UUID from an RFID (v010)
+   * Get the UUID from an RFID (v011)
    */
   //% subcategory="RFID"
   //% blockId="makerbit_rfid_get_uuid"
-  //% block="RFID UUID"
+  //% block="RFID UUID index $i"
   //% weight=89
-  export function rfidGetUUID(): number {
+  export function rfidGetUUID(i: number): number {
     let bufr = pins.i2cReadBuffer(0x24, 13, false);
-    return bufr[0];
+    return bufr[i];
   }
 
 }
