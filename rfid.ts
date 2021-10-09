6e9ea7dfe10825780604aacac1841d8f9ae4eaf2
Chris Torrence <chris_torrence@comcast.net>
2021-10-09 14:00:38 -0600

    Try to read i2c buffer

--- rfid.ts
+++ rfid.ts
@@ -6,15 +6,15 @@
 namespace makerbit {
 
   /**
-   * Get the UUID from an RFID. v005
+   * Get the UUID from an RFID. v006
    */
   //% subcategory="RFID"
   //% blockId="makerbit_rfid_get_uuid"
   //% block="RFID UUID"
   //% weight=89
   export function rfidGetUUID(): number {
-//    showStringOnLcd1602("", 1, 32);
-    return 0;
+    let bufr = pins.i2cReadBuffer(0x24, 13, false);
+    return bufr[0];
   }
 
 }
